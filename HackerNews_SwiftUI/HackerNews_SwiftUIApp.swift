8e2844ee81135172b4a6998ce4ee4fbb20c9011a
HoaiTran93 <55628172+HoaiTran93@users.noreply.github.com>
2023-05-20 10:38:23 +0800

    Initial Commit

---
+++ HackerNews_SwiftUI/HackerNews_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackerNews_SwiftUIApp.swift
+//  HackerNews_SwiftUI
+//
+//  Created by Hoài Trần on 20/5/23.
+//
+
+import SwiftUI
+
+@main
+struct HackerNews_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
